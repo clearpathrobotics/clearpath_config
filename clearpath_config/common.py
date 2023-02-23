41aecfc453b59f9aa77bb596c8c66b99623dfb43
luis-camero <lcamero@clearpathrobotics.com>
2023-02-23 14:25:14 -0500

    Added File and SerialNumber classes

--- clearpath_config/common.py
+++ clearpath_config/common.py
@@ -1,3 +1,4 @@
+import os
 import re
 
 # Hostname
@@ -95,3 +96,107 @@ class IP():
             # Fields Must be 8-Bits Wide
             field_int = int(field)
             assert 0 <= field_int < 256, "IP '%s' entries must in range 0 to 255" % ip
+
+
+# File
+# - file class
+class File():
+
+    def __init__(self, path: str, creatable=False, exists=False) -> None:
+        path = File.clean(path)
+        if creatable:
+            assert File.is_creatable(path)
+        if exists:
+            assert File.is_exists(path)
+        self.path = path
+
+    def __str__(self) -> str:
+        return self.file
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, str):
+            return self.path == other
+        elif isinstance(other, File):
+            return self.path == other.path
+        else:
+            return False
+
+    @staticmethod
+    def clean(path: str) -> str:
+        path = os.path.expanduser(path)
+        path = os.path.normpath(path)
+        path = os.path.abspath(path)
+        return path
+
+    @staticmethod
+    def is_creatable(path: str) -> bool:
+        path = File.clean(path)
+        dirname = os.path.dirname(path) or os.getcwd()
+        return os.access(dirname, os.W_OK)
+
+    @staticmethod
+    def is_exists(path: str) -> bool:
+        path = File.clean(path)
+        return os.path.exists(path)
+
+# SerialNumber
+# - Clearpath Robots Serial Number
+# - ex. cpr-j100-0100
+# - drop 'cpr' prefix as it is not required
+class SerialNumber():
+    DINGO_DIFF = "dd100"
+    DINGO_OMNI = "do100"
+    JACKAL = "j100"
+    HUSKY = "a200"
+    RIDGEBACK = "r100"
+    WARTHOG = "w200"
+    GENERIC = "generic"
+
+    MODELS = [DINGO_DIFF,
+              DINGO_OMNI,
+              JACKAL,
+              HUSKY,
+              RIDGEBACK,
+              WARTHOG,
+              GENERIC]
+
+    MODEL_NAMES = {DINGO_DIFF: "DingoD",
+                   DINGO_OMNI: "DingoO",
+                   JACKAL: "Jackal",
+                   HUSKY: "Husky",
+                   RIDGEBACK: "Ridgeback",
+                   WARTHOG: "Warthog",
+                   GENERIC: "Generic"}
+
+    def __init__(self, sn: str) -> None:
+        self.model, self.unit = SerialNumber.parse(sn)
+
+    @staticmethod
+    def parse(sn: str) -> tuple:
+        assert isinstance(sn, str), "Serial Number must be string"
+        sn = sn.lower().strip().split("-")
+        assert isinstance(1 < len(sn) < 4), "Serial Number must be delimited by hyphens ('-') and only have 3 (cpr-j100-0001) entries or 2 (j100-0001) entries"
+        # Remove CPR Prefix
+        if len(sn) == 3:
+            assert sn[0] == "cpr", "Serial Number with three fields (cpr-j100-0001) must start with cpr"
+            sn = sn[1:]
+        # Match to Robot
+        assert sn[0] in SerialNumber.MODELS, "Serial Number model entry must match one of %s" % SerialNumber.MODELS
+        # Check Number
+        assert sn[1].isdecimal(), "Serial Number unit entry must be an integer value"
+        return (sn[0], sn[1])
+
+    def get_model(self) -> str:
+        return self.model
+
+    def get_model_name(self) -> str:
+        return self.MODEL_NAMES[self.model]
+
+    def get_unit(self) -> str:
+        return self.unit
+
+    def get_serial(self, prefix = False) -> str:
+        if prefix:
+            return "-".join(["cpr", self.model, self.unit])
+        else:
+            return "-".join([self.model, self.unit])
