71372a807c646f68dbc8640d1deb985489a23943
luis-camero <lcamero@clearpathrobotics.com>
2023-02-28 22:12:00 -0500

    Removed parsing from clearpath_config

--- clearpath_config/clearpath_config.py
+++ clearpath_config/clearpath_config.py
@@ -1,6 +1,5 @@
-from clearpath_config.parser import ConfigParser
-from clearpath_config.system import SystemConfig, Host
-from clearpath_config.utils import read_yaml, write_yaml
+from clearpath_config.system import SystemConfig
+from clearpath_config.platform import PlatformConfig
 
 # ClearpathConfig: 
 #  - top level configurator
@@ -10,14 +9,7 @@ class ClearpathConfig():
     def __init__(self, config: dict = None) -> None:
         self.config = config
         self.version = 0
-        self.system = ConfigParser.load_system_config(config)
-        #self.platform = PlatformConfig()
+        self.system = SystemConfig()
+        self.platform = PlatformConfig()
         #self.mounts = MountsConfig()
         #self.sensors = SensorsConfig()
-
-    def load_config(self, config: dict):
-        self.config = config
-
-    def export_config(self) -> dict:
-        config = {}
-        return config
