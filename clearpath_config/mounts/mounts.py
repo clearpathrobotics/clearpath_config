3012a170dc9da1c8d9946a36320496e0eca87548
Luis Camero <lcamero@clearpathrobotics.com>
2023-03-16 14:16:29 -0400

    Fixed lint errors in mounts

--- clearpath_config/mounts/mounts.py
+++ clearpath_config/mounts/mounts.py
@@ -2,7 +2,8 @@ from clearpath_config.common import Accessory, File, IP, List
 from copy import deepcopy
 from math import pi
 
-class Mount():
+
+class Mount:
     FATH_PIVOT = "fath_pivot"
     FLIR_PTU = "flir_ptu"
     ALL = [FATH_PIVOT, FLIR_PTU]
@@ -11,14 +12,14 @@ class Mount():
         MOUNTING_LINK = None
 
         def __init__(
-                self,
-                name: str,
-                model: str,
-                parent: str = Accessory.PARENT,
-                mounting_link: str = MOUNTING_LINK,
-                xyz: List[float] = Accessory.XYZ,
-                rpy: List[float] = Accessory.RPY,
-            ) -> None:
+            self,
+            name: str,
+            model: str,
+            parent: str = Accessory.PARENT,
+            mounting_link: str = MOUNTING_LINK,
+            xyz: List[float] = Accessory.XYZ,
+            rpy: List[float] = Accessory.RPY,
+        ) -> None:
             super().__init__(name, parent, xyz, rpy)
             self.model = str()
             self.set_model(model)
@@ -30,8 +31,10 @@ class Mount():
             return self.model
 
         def set_model(self, model: str) -> None:
-            assert (model in Mount.ALL
-                ), "Model '%s' must be one of '%s'" % (model, self.MODELS)
+            assert model in Mount.ALL, "Model '%s' must be one of '%s'" % (
+                model,
+                self.MODELS,
+            )
             self.model = model
 
         def get_mounting_link(self) -> str:
@@ -41,21 +44,26 @@ class Mount():
             self.assert_valid_link(mounting_link)
             self.mounting_link = mounting_link
 
-
     class FathPivot(Base):
         MOUNTING_LINK = None
         ANGLE = 0.0
 
         def __init__(
-                self,
-                name: str,
-                parent: str = Accessory.PARENT,
-                mounting_link: str = MOUNTING_LINK,
-                angle: float = ANGLE,
-                xyz: List[float] = Accessory.XYZ,
-                rpy: List[float] = Accessory.RPY,
-            ) -> None:
-            super().__init__(name, Mount.FATH_PIVOT, parent, mounting_link, xyz, rpy)
+            self,
+            name: str,
+            parent: str = Accessory.PARENT,
+            mounting_link: str = MOUNTING_LINK,
+            angle: float = ANGLE,
+            xyz: List[float] = Accessory.XYZ,
+            rpy: List[float] = Accessory.RPY,
+        ) -> None:
+            super().__init__(
+                name,
+                Mount.FATH_PIVOT,
+                parent,
+                mounting_link,
+                xyz,
+                rpy)
             self.angle = 0.0
             if angle:
                 self.set_angle(angle)
@@ -64,10 +72,10 @@ class Mount():
             return self.angle
 
         def set_angle(self, angle: float) -> None:
-            assert(-pi < angle <= pi
-                ), "Angle '%s' must be in radian and  between pi and -pi"
+            assert -pi < angle <= pi, (
+                "Angle '%s' must be in radian and  between pi and -pi"
+            )
             self.angle = angle
-
 
     class FlirPTU(Base):
         # Default Values
@@ -84,19 +92,26 @@ class Mount():
         CONNECTION_TYPES = [TTY, TCP]
 
         def __init__(
-                self,
-                name: str,
-                parent: str = Accessory.PARENT,
-                mounting_link: str = MOUNTING_LINK,
-                xyz: List[float] = Accessory.XYZ,
-                rpy: List[float] = Accessory.RPY,
-                tty_port: str = TTY_PORT,
-                tcp_port: int = TCP_PORT,
-                ip: str = IP_ADDRESS,
-                connection_type: str = CONNECTION_TYPE,
-                limits_enabled: bool = LIMITS_ENABLED,
-            ) -> None:
-            super().__init__(name, Mount.FLIR_PTU, parent, mounting_link, xyz, rpy)
+            self,
+            name: str,
+            parent: str = Accessory.PARENT,
+            mounting_link: str = MOUNTING_LINK,
+            xyz: List[float] = Accessory.XYZ,
+            rpy: List[float] = Accessory.RPY,
+            tty_port: str = TTY_PORT,
+            tcp_port: int = TCP_PORT,
+            ip: str = IP_ADDRESS,
+            connection_type: str = CONNECTION_TYPE,
+            limits_enabled: bool = LIMITS_ENABLED,
+        ) -> None:
+            super().__init__(
+                name,
+                Mount.FLIR_PTU,
+                parent,
+                mounting_link,
+                xyz,
+                rpy,
+            )
             # Serial Port
             self.tty_port = File(self.TTY_PORT)
             self.set_tty_port(tty_port)
@@ -123,8 +138,9 @@ class Mount():
             return self.tcp_port
 
         def set_tcp_port(self, tcp_port: int) -> None:
-            assert(1024 < tcp_port < 65536
-                ), "TCP port '%s' must be in range 1024 to 65536" % tcp_port
+            assert 1024 < tcp_port < 65536, (
+                "TCP port '%s' must be in range 1024 to 65536" % tcp_port
+            )
             self.tcp_port = tcp_port
 
         def get_ip(self) -> str:
@@ -137,10 +153,12 @@ class Mount():
             return self.connection_type
 
         def set_connection_type(self, connection_type: str) -> None:
-            assert(connection_type in self.CONNECTION_TYPES
-                ), "Connection type '%s' must be one of '%s'" % (
-                connection_type, self.CONNECTION_TYPES
+            assert connection_type in self.CONNECTION_TYPES, (
+                "Connection type '%s' must be one of '%s'" % (
+                    connection_type,
+                    self.CONNECTION_TYPES,
                 )
+            )
             self.connection_type = connection_type
 
         def get_limits_enabled(self) -> bool:
@@ -152,14 +170,13 @@ class Mount():
     MODEL = {
         FATH_PIVOT: FathPivot,
         FLIR_PTU: FlirPTU,
-        }
+    }
 
     def __new__(cls, name: str, model: str) -> Base:
         return Mount.MODEL[model](name)
 
 
 class MountsConfig:
-
     def __init__(self, mounts: List[Mount.Base] = []) -> None:
         self.mounts = list()
         self.set_mounts(mounts)
@@ -168,25 +185,34 @@ class MountsConfig:
         return self.mounts
 
     def set_mounts(self, mounts: List[Mount.Base]) -> None:
-        assert (isinstance(mounts, list)
-            ), "Mounts must be a list of Mount objects"
-        assert (all([isinstance(m, Mount.Base) for m in mounts])
-            ), "Mounts must be a list of Mount objects"
+        assert isinstance(mounts, list), (
+            "Mounts must be a list of Mount objects"
+        )
+        assert all([isinstance(m, Mount.Base) for m in mounts]), (
+            "Mounts must be a list of Mount objects"
+        )
         temp = deepcopy(self.mounts)
         self.mounts.clear()
         for mount in mounts:
-            self.add_mount(mount)
+            try:
+                self.add_mount(mount)
+            except AssertionError as e:
+                self.mounts = temp
+                raise e
 
     def add_mount(self, mount: Mount.Base) -> None:
-        assert (isinstance(mount, Mount.Base)
-            ), "Mount '%s' must be of type Mount." % mount
-        assert (mount.get_name() not in [m.get_name() for m in self.mounts]
-            ), "Mount name '%s' must be unique." % mount.get_name()
+        assert isinstance(mount, Mount.Base), (
+            "Mount '%s' must be of type Mount." % mount
+        )
+        assert mount.get_name() not in [m.get_name() for m in self.mounts], (
+            "Mount name '%s' must be unique." % mount.get_name()
+        )
         self.mounts.append(mount)
 
     def remove_mount(self, mount: Mount.Base = None, name: str = None) -> None:
-        assert (mount or name
-            ), "Mount or name of mount must be passed to 'remove_mount'"
+        assert mount or name, (
+            "Mount or name of mount must be passed to 'remove_mount'"
+        )
         if mount:
             name = mount.get_name()
         for mount in self.mounts:
