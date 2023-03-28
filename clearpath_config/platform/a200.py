5902e23578af4f0381fb3fe1518abc51ddc9366c
Luis Camero <lcamero@clearpathrobotics.com>
2023-03-28 17:07:03 -0400

    Updated A200 to iterable decorations

--- clearpath_config/platform/a200.py
+++ clearpath_config/platform/a200.py
@@ -1,28 +1,31 @@
 # A200 Husky Platform Configuration
 from clearpath_config.common import Platform
-from clearpath_config.platform.decorations import BaseDecorationsConfig, Decorations
-from clearpath_config.platform.pacs import (
-    BracketsConfig,
-    FullRisersConfig,
-    RowRisersConfig,
-)
-
-
-# A200 Husky PACS Configuration
-class A200PACSConfig(BracketsConfig, FullRisersConfig, RowRisersConfig):
-    def __init__(self) -> None:
-        super().__init__()
+from clearpath_config.platform.base import BaseDecorationsConfig
+from clearpath_config.platform.decorations import Decorations
 
 
 # A200 Husky Decorations Configuration
 class A200DecorationsConfig(BaseDecorationsConfig):
+
     def __init__(self) -> None:
         super().__init__(model=Platform.A200)
-        self.front_bumper = Decorations.Bumper(
-            enable=True, extension=0.0, model=Decorations.Bumper.DEFAULT
+        # Front Bumper
+        self.add_bumper(
+            name="front_bumper",
+            enable=True,
+            extension=0.0,
+            model=Decorations.Bumper.DEFAULT
         )
-        self.rear_bumper = Decorations.Bumper(
-            enable=True, extension=0.0, model=Decorations.Bumper.DEFAULT
+        # Rear Bumper
+        self.add_bumper(
+            name="rear_bumper",
+            enable=True,
+            extension=0.0,
+            model=Decorations.Bumper.DEFAULT
         )
-        self.top_plate = Decorations.A200.TopPlate()
-        self.pacs = A200PACSConfig()
+        # Top Plate
+        self.add_top_plate(
+            name="top_plate",
+            enable=True,
+            model=Decorations.TopPlate.DEFAULT
+        )
