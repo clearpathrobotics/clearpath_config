e54062c9d62ffa573b49a9c50297ce36bc73997d
Luis Camero <lcamero@clearpathrobotics.com>
2023-03-28 17:09:04 -0400

    Updated J100 to iterable decorations

--- clearpath_config/platform/j100.py
+++ clearpath_config/platform/j100.py
@@ -1,23 +1,24 @@
 # J100 Jackal Platform Configuration
 from clearpath_config.common import Platform
-from clearpath_config.platform.decorations import BaseDecorationsConfig, Decorations
-
-
-# J100 Jackal PACS Configuration
-class J100PACSConfig:
-    def __init__(self) -> None:
-        super().__init__()
+from clearpath_config.platform.base import BaseDecorationsConfig
+from clearpath_config.platform.decorations import Decorations
 
 
 # J100 Jackal Decorations Configuration
 class J100DecorationsConfig(BaseDecorationsConfig):
     def __init__(self) -> None:
         super().__init__(model=Platform.J100)
-        self.front_bumper = Decorations.Bumper(
-            enable=True, extension=0.0, model=Decorations.Bumper.DEFAULT
+        # Front Bumper
+        self.add_bumper(
+            name="front_bumper",
+            enable=True,
+            extension=0.0,
+            model=Decorations.Bumper.DEFAULT
         )
-        self.rear_bumper = Decorations.Bumper(
-            enable=True, extension=0.0, model=Decorations.Bumper.DEFAULT
+        # Rear Bumper
+        self.add_bumper(
+            name="rear_bumper",
+            enable=True,
+            extension=0.0,
+            model=Decorations.Bumper.DEFAULT
         )
-        # No J100 Jackal PACS Config Yet
-        # self.pacs = J100PACSConfig()
