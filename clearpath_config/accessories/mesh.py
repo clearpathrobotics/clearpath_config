fda1fac93cccc46568efbe7a972ea740338df1fe
Luis Camero <lcamero@clearpathrobotics.com>
2023-05-02 12:00:05 -0400

    Added visual file path

--- clearpath_config/accessories/mesh.py
+++ clearpath_config/accessories/mesh.py
@@ -1,4 +1,4 @@
-from clearpath_config.common import Accessory
+from clearpath_config.common import Accessory, File
 from clearpath_config.accessories.base import BaseAccessory
 from typing import List
 
@@ -27,7 +27,8 @@ class Mesh(BaseAccessory):
             offset_xyz,
             offset_rpy
         )
-        self.visual = Fil
+        self.visual: File = File(Mesh.VISUAL)
+        self.set_visual(visual)
 
     def set_radius(self, radius: float) -> None:
         msg = "Radius must be a positive float value"
@@ -37,3 +38,9 @@ class Mesh(BaseAccessory):
 
     def get_radius(self) -> float:
         return self.radius
+
+    def set_visual(self, visual: str) -> None:
+        self.visual = File(visual)
+
+    def get_visual(self) -> str:
+        return self.visual.get_path()
