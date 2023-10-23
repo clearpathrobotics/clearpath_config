30db3756e031c0f625427be5c142a6751c7021f7
Luis Camero <lcamero@clearpathrobotics.com>
2023-10-23 17:20:47 -0400

    Update commit

---
+++ clearpath_config/platform/battery.py
@@ -0,0 +1,155 @@
+# Software License Agreement (BSD)
+#
+# @author    Luis Camero <lcamero@clearpathrobotics.com>
+# @copyright (c) 2023, Clearpath Robotics, Inc., All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of Clearpath Robotics nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+from clearpath_config.common.types.config import BaseConfig
+from clearpath_config.common.types.platform import Platform
+from clearpath_config.common.utils.dictionary import flip_dict
+
+
+class BatteryConfig(BaseConfig):
+    BATTERY = "battery"
+
+    # Models
+    MODEL = "model"
+    UNKNOWN = "unknown"
+    # Husky Lead Acid
+    ES20_12C = "ES20-12C"
+    # Husky/Jackal Li_ION
+    HE2613 = "HE2613"
+    # Warthog Lead Acid
+    U1_35 = "U1-35"
+    # Warthog LiFEPO4
+    ALM12V35 = "ALM12V35"
+    U24_12XP = "U24-12XP"
+    U27_12XP = "U27-12XP"
+
+    # Configurations
+    CONFIGURATION = "configuration"
+    _2S1P = "2S1P"
+    _1S3P = "1S3P"
+    _1S4P = "1S4P"
+    _1S1P = "1S1P"
+    _4S3P = "4S3P"
+    _4S1P = "4S1P"
+
+    VALID = {
+        Platform.GENERIC: {
+            UNKNOWN: [UNKNOWN]
+        },
+        Platform.A200: {
+            ES20_12C: [_2S1P],
+            HE2613: [_1S3P, _1S4P],
+        },
+        Platform.J100: {
+            HE2613: [_1S1P],
+        },
+        Platform.W200: {
+            U1_35: [_4S3P],
+            ALM12V35: [_4S3P],
+            U24_12XP: [_4S1P],
+            U27_12XP: [_4S1P],
+        },
+    }
+
+    TEMPLATE = {
+        BATTERY: {
+            MODEL: MODEL,
+            CONFIGURATION: CONFIGURATION
+        }
+    }
+
+    KEYS = flip_dict(TEMPLATE)
+
+    DEFAULTS = {
+        MODEL: UNKNOWN,
+        CONFIGURATION: UNKNOWN
+    }
+
+    def __init__(
+            self,
+            config: dict = {},
+            model: str = DEFAULTS[MODEL],
+            configuration: str = DEFAULTS[CONFIGURATION],
+            ) -> None:
+        # Initialization
+        self._config = {}
+        self.model = model
+        self.configuration = configuration
+        # Setter Template
+        setters = {
+            self.KEYS[self.MODEL]: BatteryConfig.model,
+            self.KEYS[self.CONFIGURATION]: BatteryConfig.configuration,
+        }
+        super().__init__(setters, config, self.BATTERY)
+
+    def update(self, serial_number: bool = False) -> None:
+        if serial_number:
+            platform = BaseConfig.get_platform_model()
+            self.model = list(self.VALID[platform])[0]
+            self.configuration = list(self.VALID[platform][self.model])[0]
+
+    @property
+    def model(self) -> str:
+        self.set_config_param(
+            key=self.KEYS[self.MODEL],
+            value=self._model
+        )
+        return self._model
+
+    @model.setter
+    def model(self, value: str) -> None:
+        platform = BaseConfig.get_platform_model()
+        assert platform in self.VALID, (
+            "Platform must be one of: %s" % self.VALID
+        )
+        assert value in self.VALID[platform], (
+            "Battery model for platform '%s' must be one of: %s" % (platform, self.VALID[platform])
+        )
+        self._model = value
+
+    @property
+    def configuration(self) -> str:
+        self.set_config_param(
+            key=self.KEYS[self.CONFIGURATION],
+            value=self._configuration
+        )
+        return self._configuration
+
+    @configuration.setter
+    def configuration(self, value: str) -> None:
+        platform = BaseConfig.get_platform_model()
+        assert platform in self.VALID, (
+            "Platform must be one of: %s" % self.VALID
+        )
+        assert self.model in self.VALID[platform], (
+            "Battery model for platform '%s' must be one of: %s" % (platform, self.VALID[platform])
+        )
+        assert value in self.VALID[platform][self.model], (
+            "Battery configuration for platform '%s', and battery model '%s' must be one of: %s" % (
+                platform, self.model, self.VALID[platform][self.model])
+        )
+        self._configuration = value
