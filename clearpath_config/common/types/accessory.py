6363850641a3b05df9784bae8f8abb93e4a2f1fc
Luis Camero <lcamero@clearpathrobotics.com>
2023-11-03 10:06:38 -0400

    Accessory from/to dict

--- clearpath_config/common/types/accessory.py
+++ clearpath_config/common/types/accessory.py
@@ -50,6 +50,24 @@ class Accessory():
         self.set_xyz(xyz)
         self.set_rpy(rpy)
 
+    def to_dict(self) -> dict:
+        return {
+            'name': self.get_name(),
+            'parent': self.get_parent(),
+            'xyz': self.get_xyz(),
+            'rpy': self.get_rpy(),
+        }
+
+    def from_dict(self, d: dict) -> None:
+        if 'name' in d:
+            self.set_name(d['name'])
+        if 'parent' in d:
+            self.set_parent(d['parent'])
+        if 'xyz' in d:
+            self.set_xyz(d['xyz'])
+        if 'rpy' in d:
+            self.set_rpy(d['rpy'])
+
     def get_name(self) -> str:
         return self.name
 
