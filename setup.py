01bcb566e279637b8a0357db12b596a866e13034
Tony Baltovski <tbaltovski@clearpathrobotics.com>
2024-01-04 15:16:40 -0500

    0.2.1

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ package_name = "clearpath_config"
 
 setup(
     name=package_name,
-    version="0.2.0",
+    version="0.2.1",
     packages=[
         package_name,
         package_name + ".common",
