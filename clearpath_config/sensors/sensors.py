6699590ce989505142e768b308b2cf5672bd8c3f
Luis Camero <lcamero@clearpathrobotics.com>
2025-03-10 15:02:23 +0000

    Alphabetical entries to Lidar3D models

--- clearpath_config/sensors/sensors.py
+++ clearpath_config/sensors/sensors.py
@@ -157,14 +157,14 @@ class Lidar2D():
 
 
 class Lidar3D():
-    VELODYNE_LIDAR = VelodyneLidar.SENSOR_MODEL
     OUSTER_OS1 = OusterOS1.SENSOR_MODEL
     SEYOND_LIDAR = SeyondLidar.SENSOR_MODEL
+    VELODYNE_LIDAR = VelodyneLidar.SENSOR_MODEL
 
     MODEL = {
-        VELODYNE_LIDAR: VelodyneLidar,
         OUSTER_OS1: OusterOS1,
         SEYOND_LIDAR: SeyondLidar,
+        VELODYNE_LIDAR: VelodyneLidar,
     }
 
     @classmethod
