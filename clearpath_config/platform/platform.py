b0c19fcf67933a7b6a0d06725e1034898c462052
Luis Camero <lcamero@clearpathrobotics.com>
2025-05-26 16:36:50 -0400

    Add foxglove bridge enable to platform section

--- clearpath_config/platform/platform.py
+++ clearpath_config/platform/platform.py
@@ -119,6 +119,9 @@ class PlatformConfig(BaseConfig):
     # Enable/disable EKF
     ENABLE_EKF = 'enable_ekf'
 
+    # Enable/disable Foxglove bridge
+    ENABLE_FOXGLOVE_BRIDGE = 'enable_foxglove_bridge'
+
     TEMPLATE = {
         PLATFORM: {
             CONTROLLER: CONTROLLER,
@@ -131,7 +134,8 @@ class PlatformConfig(BaseConfig):
             CONTROL: CONTROL,
             BATTERY: BATTERY,
             WHEEL: WHEEL,
-            ENABLE_EKF: ENABLE_EKF
+            ENABLE_EKF: ENABLE_EKF,
+            ENABLE_FOXGLOVE_BRIDGE: ENABLE_FOXGLOVE_BRIDGE,
         }
     }
 
@@ -150,6 +154,7 @@ class PlatformConfig(BaseConfig):
         BATTERY: BatteryConfig.DEFAULTS,
         WHEEL: 'default',
         ENABLE_EKF: True,
+        ENABLE_FOXGLOVE_BRIDGE: False,
     }
 
     def __init__(
@@ -163,6 +168,7 @@ class PlatformConfig(BaseConfig):
             extras: dict = DEFAULTS[EXTRAS],
             wheel: dict = DEFAULTS[WHEEL],
             enable_ekf: bool = DEFAULTS[ENABLE_EKF],
+            enable_foxglove_bridge: bool = DEFAULTS[ENABLE_FOXGLOVE_BRIDGE],
             ) -> None:
         # Initialization
         self._config = {}
@@ -177,6 +183,7 @@ class PlatformConfig(BaseConfig):
         self.control = self.DEFAULTS[self.CONTROL]
         self.wheel = wheel
         self.enable_ekf = enable_ekf
+        self.enable_foxglove_bridge = enable_foxglove_bridge
         # Setter Template
         setters = {
             self.KEYS[self.CONTROLLER]: PlatformConfig.controller,
@@ -186,7 +193,8 @@ class PlatformConfig(BaseConfig):
             self.KEYS[self.BATTERY]: PlatformConfig.battery,
             self.KEYS[self.EXTRAS]: PlatformConfig.extras,
             self.KEYS[self.WHEEL]: PlatformConfig.wheel,
-            self.KEYS[self.ENABLE_EKF]: PlatformConfig.enable_ekf
+            self.KEYS[self.ENABLE_EKF]: PlatformConfig.enable_ekf,
+            self.KEYS[self.ENABLE_FOXGLOVE_BRIDGE]: PlatformConfig.enable_foxglove_bridge,
         }
         super().__init__(setters, config, self.PLATFORM)
 
@@ -385,3 +393,15 @@ class PlatformConfig(BaseConfig):
     @enable_ekf.setter
     def enable_ekf(self, value: bool) -> None:
         self._enable_ekf = value
+
+    @property
+    def enable_foxglove_bridge(self) -> bool:
+        self.set_config_param(
+            key=self.KEYS[self.ENABLE_FOXGLOVE_BRIDGE],
+            value=self._enable_foxglove_bridge
+        )
+        return self._enable_foxglove_bridge
+
+    @enable_foxglove_bridge.setter
+    def enable_foxglove_bridge(self, value: bool) -> None:
+        self._enable_foxglove_bridge = value
